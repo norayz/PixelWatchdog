387317048c6efa851151972ced6cc9bd33f145ef
yaronleiby <yaron5594@gmail.com>
2016-06-24 19:41:35 +0300

    working

--- PixelWatchdog.py
+++ PixelWatchdog.py
@@ -1,12 +1,47 @@
-def pxlWatchdog(serviceName,
-                isSerivceHealthyCallback, serverIsHealthyCallback=None, serviceNotHealthyCallback=None,
-                maxRetries=3, retryTimeoutMS=1000):
-    if (isSerivceHealthyCallback()):
-        print(serviceName + ' is healthy')
-    else:
-        print(serviceName + ' not healthy')
+import time
+import win32serviceutil
+
+#TODO: add logger..
+
+def pxlRestartServiceIfNotHealthy(serviceName,
+                isServiceHealthyCallback,
+                serviceIsHealthyCallback=None,
+                serviceNotHealthyCallback=None,
+                maxRetries=3, retryTimeoutSec=1):
+
+    currentRetryNum = 0
+
+    while (currentRetryNum < maxRetries):
+        print('Retry number {0}:'.format(str(currentRetryNum)))
+
+        if (isSerivceHealthyCallback()):
+            print("- Service: '{0}' is OK".format(serviceName))
+            serviceIsHealthyCallback()
+            break
+
+        print("- Service: '{0}' is NOT responding".format(serviceName))
+        time.sleep(retryTimeoutSec)
+        currentRetryNum += 1
+
+    if (currentRetryNum >= maxRetries):
+        print("Service: '{0}' is not healthy".format(serviceName))
+        serviceNotHealthyCallback()
+        _pxlRestartService(serviceName)
+
+
+def _pxlRestartService(serviceName):
+    print("Trying to restart service: '{0}'".format(serviceName))
+    try:
+        win32serviceutil.RestartService(serviceName)
+        print("'{0}' is restarted".format(serviceName))
+    except win32serviceutil.error as e:
+        print("Could not restart service: '{0}'".format(serviceName))
+        print(e)
+
+def serviceNotHealthyCallback():
+    print('i am ill, efshar gimel?')
 
 def isSerivceHealthyCallback():
-    return True
+    return False
 
-pxlWatchdog('yaronsss', isSerivceHealthyCallback)
+pxlRestartServiceIfNotHealthy('XboxNetApiSvc', isSerivceHealthyCallback, serviceNotHealthyCallback=serviceNotHealthyCallback)
